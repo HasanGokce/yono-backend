353c89d1d5985d4caefbee2d4491d4c21076474a
Hasan Gökçe <smallsteps@Hasan-Air.home>
2024-04-27 23:07:14 +0300

    Socket path added

--- ms-web/app/utils/socket.ts
+++ ms-web/app/utils/socket.ts
@@ -2,4 +2,4 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL as string, {path: '/your-path-here'});
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL as string, {path: process.env.NEXT_PUBLIC_SOCKET_PATH});
