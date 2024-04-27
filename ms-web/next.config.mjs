5e6dcbb13cddaa991320b3bda9dfb827f206fdfd
Hasan Gökçe <smallsteps@Hasan-Air.home>
2024-04-27 22:47:22 +0300

    process.env doesnt work on next.config.mjs

--- ms-web/next.config.mjs
+++ ms-web/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/mvp/web",
 };
 
 export default nextConfig;
