347aa281fd85caa0fe001cefe5fa39bdf4d42302
Hasan Gökçe <smallsteps@Hasan-Air.home>
2024-05-10 00:45:11 +0300

    Game detail page updated

--- ms-web/app/components/pages/GameWelcomePage.tsx
+++ ms-web/app/components/pages/GameWelcomePage.tsx
@@ -24,8 +24,8 @@ export default function GameWelcomePage(props: { gameData: GameInfo }) {
   const imageUrl = process.env.NEXT_PUBLIC_STRAPI_API_URL + thumbnailUrl;
   return (
     <div className="relative flex min-h-screen justify-center overflow-hidden mt-4 pl-2 pr-2">
-      <div className="relative px-0 pt-0 pb-0 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-        <div className="mx-auto max-w-md">
+      <div>
+        <div>
           <Title>{name}</Title>
           <Image
             src={imageUrl}
