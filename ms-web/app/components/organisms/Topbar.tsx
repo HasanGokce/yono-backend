e2407c5f8edf502912b09bc14dd1d0e729ab0893
Hasan Gökçe <smallsteps@Hasan-Air.home>
2024-05-15 04:42:21 +0300

    Logo removed

--- ms-web/app/components/organisms/Topbar.tsx
+++ ms-web/app/components/organisms/Topbar.tsx
@@ -1,35 +1,35 @@
 "use client";
-import Image from "next/image";
+// import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export function Topbar() {
   const router = useRouter();
+  const currentURL = usePathname();
+
   return (
     <div className="flex items-center justify-center pt-2 pb-2">
-      <div className="text-neutral-100 flex-1">
-        <span
-          onClick={(e) => {
-            router.back();
-          }}
+      {/* Koşullu olarak bir şey gösterme */}
+      {currentURL === "/home" ? (
+        <div>{/* Logo veya diğer içerikler buraya gelebilir */}</div>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6 ml-2"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 19.5 8.25 12l7.5-7.5"
-            />
-          </svg>
-        </span>
-      </div>
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+          />
+        </svg>
+      )}
       <div>
-        <p className="left-0 top-0 flex w-full justify-center pb-0 pt-0 lg:p-0">
+        {/* <p className="left-0 top-0 flex w-full justify-center pb-0 pt-0 lg:p-0">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-2 lg:pointer-events-auto lg:p-0"
             href="#"
@@ -45,7 +45,7 @@ export function Topbar() {
               priority={true}
             />
           </a>
-        </p>
+        </p> */}
       </div>
       <div className="flex-1">
         {/* <div className="w-20 bg-green-500 ml-auto"></div> */}
